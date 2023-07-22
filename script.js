76cb6d904b849f96b86fbb3aee12111f5b9fb692
shutterlock07 <65783074+shutterlock07@users.noreply.github.com>
2023-07-22 12:21:42 +0530

    done/undone functionality

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ let tasks = [];
 function addTask() {
     const taskText = taskInput.value.trim();
     if (taskText !== "") {
-        tasks.push(taskText);
+        const task = {
+            text: taskText,
+            done: false,
+        };
+        tasks.push(task);
         updateTaskList();
         taskInput.value = "";
     }
@@ -21,11 +25,17 @@ function removeTask(index) {
 
 // Function to edit a task in the todo list
 function editTask(index) {
-    const newTaskText = prompt("Edit task:", tasks[index]);
+    const newTaskText = prompt("Edit task:", tasks[index].text);
     if (newTaskText !== null) {
-        tasks[index] = newTaskText.trim();
+        tasks[index].text = newTaskText.trim();
         updateTaskList();
     }
+}
+
+// Function to toggle done/undone for the task
+function statusToggle(index) {
+    tasks[index].done = !tasks[index].done;
+    updateTaskList();
 }
 
 // Function to update the task list in the UI
@@ -33,7 +43,14 @@ function updateTaskList() {
     taskList.innerHTML = "";
     tasks.forEach((task, index) => {
         const li = document.createElement("li");
-        li.textContent = task;
+        li.className = "task-item";
+
+        const taskTextElement = document.createElement("span");
+        taskTextElement.textContent = task.text;
+        taskTextElement.classList.add("task-text");
+        if (task.done) {
+            taskTextElement.classList.add("done");
+        }
 
         const actionsContainer = document.createElement("div");
         actionsContainer.className = "actions";
@@ -43,13 +60,24 @@ function updateTaskList() {
         editBtn.className = "edit-btn";
         editBtn.addEventListener("click", () => editTask(index));
 
+        const statusBtn = document.createElement("button");
+        statusBtn.textContent = task.done ? "Undone" : "Done";
+        statusBtn.className = "status-btn";
+        statusBtn.addEventListener("click", () => statusToggle(index));
+
         const deleteBtn = document.createElement("button");
         deleteBtn.textContent = "Delete";
         deleteBtn.className = "delete-btn";
         deleteBtn.addEventListener("click", () => removeTask(index));
+
+
         actionsContainer.appendChild(editBtn);
+        actionsContainer.appendChild(statusBtn);
         actionsContainer.appendChild(deleteBtn);
+
+        li.appendChild(taskTextElement);
         li.appendChild(actionsContainer);
+
         taskList.appendChild(li);
     });
 }
@@ -60,39 +88,40 @@ function updateTaskList() {
 
 
 // Initial rendering of the task list
-// updateTaskList();
+updateTaskList();
 
-function fetchTasks() {
-    // Simulate API call with a delay of 1 second
-    setTimeout(() => {
-        // Replace this with your actual API endpoint or data retrieval logic
-        tasks = [];
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not OK');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                // Process the received data 
+// function fetchTasks() {
+//     // Simulate API call with a delay of 1 second
+//     setTimeout(() => {
+//         // Replace this with your actual API endpoint or data retrieval logic
+//         tasks = [];
+//         // https://jsonplaceholder.typicode.com/todos
+//         fetch('')
+//             .then((response) => {
+//                 if (!response.ok) {
+//                     throw new Error('Network response was not OK');
+//                 }
+//                 return response.json();
+//             })
+//             .then((data) => {
+//                 // Process the received data
 
-                [...data].forEach(ele => {
-                    tasks.push([ele.title]);
+//                 [...data].forEach(ele => {
+//                     tasks.push([ele.title]);
 
-                    // localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
-                });
-                console.log();
-                updateTaskList(tasks);
-            })
-            .catch(error => {
-                // Handle any errors that occurred during the fetch request 
-                console.log('Error:', error.message);
-            })
-        updateTaskList();
-    }, 1000);
-}
+//                     // localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray))
+//                 });
+//                 console.log();
+//                 updateTaskList(tasks);
+//             })
+//             .catch(error => {
+//                 // Handle any errors that occurred during the fetch request
+//                 console.log('Error:', error.message);
+//             })
+//         updateTaskList();
+//     }, 1000);
+// }
 
 
-// Fetch tasks from the API on page load
-fetchTasks();
+// // Fetch tasks from the API on page load
+// fetchTasks();
